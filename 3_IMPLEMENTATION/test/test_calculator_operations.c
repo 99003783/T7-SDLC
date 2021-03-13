5d00ec9e379d648c6737040dcd58a2aee9412774
99003778 <kopparapu.rani@ltts.com>
2021-03-13 17:19:11 +0530

    updated code

--- 3_IMPLEMENTATION/test/test_calculator_operations.c
+++ 3_IMPLEMENTATION/test/test_calculator_operations.c
@@ -14,6 +14,38 @@ void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
 void test_modulus(void);
+
+
+
+
+//trignometric test functions
+
+//Test Case1 For Trignometry
+void test_Sin_func1(void);
+void test_Cos_func1(void);
+void test_Tan_func1(void);
+void test_Cosec_func1(void);
+void test_Sec_func1(void);
+void test_Cot_func1(void);
+//Test Case2 For Trignometry
+void test_Sin_func2(void);
+void test_Cos_func2(void);
+void test_Tan_func2(void);
+void test_Cosec_func2(void);
+void test_Sec_func2(void);
+void test_Cot_func2(void);
+//Test Case3 For Trignometry
+void test_Sin_func3(void);
+void test_Cos_func3(void);
+void test_Tan_func3(void);
+void test_Cosec_func3(void);
+void test_Sec_func3(void);
+void test_Cot_func3(void);
+
+
+//cube root test functions
+void test_cube_root(void);
+
 //void test_squareroot(void);
 //void test_btod(void);
 //void test_power(void);
@@ -37,6 +69,30 @@ int main()
   RUN_TEST(test_multiply);
   RUN_TEST(test_divide);
   RUN_TEST(test_modulus);
+
+
+    RUN_TEST(test_Sin_func1);
+    RUN_TEST(test_Cos_func1);
+    RUN_TEST(test_Cosec_func1);
+    RUN_TEST(test_Sec_func1);
+    RUN_TEST(test_Tan_func1);
+    RUN_TEST(test_Cot_func1);
+
+    RUN_TEST(test_Sin_func2);
+    RUN_TEST(test_Cos_func2);
+    RUN_TEST(test_Cosec_func2);
+    RUN_TEST(test_Sec_func2);
+    RUN_TEST(test_Tan_func2);
+    RUN_TEST(test_Cot_func2);
+    
+    RUN_TEST(test_Sin_func3);
+    RUN_TEST(test_Cos_func3);
+    RUN_TEST(test_Cosec_func3);
+    RUN_TEST(test_Sec_func3);
+    RUN_TEST(test_Tan_func3);
+    RUN_TEST(test_Cot_func3);
+    
+    RUN_TEST(test_cube_root);
   //RUN_TEST(test_squareroot);
   //RUN_TEST(test_btod);
   //RUN_TEST(test_power);
@@ -81,6 +137,87 @@ void test_modulus(void){
   TEST_ASSERT_EQUAL(0,modulus(10,5));
 }
 
+
+
+void test_Sin_func1(void)
+{
+     TEST_ASSERT_EQUAL(0.5,Sin_func(30));
+}
+
+void test_Cos_func1(void)
+{
+     TEST_ASSERT_EQUAL(1,Cos_func(30));
+}
+void test_Tan_func1(void)
+{
+      TEST_ASSERT_EQUAL(0,Tan_func(30));
+}
+void test_Cosec_func1(void)
+{
+      TEST_ASSERT_EQUAL(-2147483648,Cosec_func(30));
+}
+void test_Sec_func1(void)
+{
+      TEST_ASSERT_EQUAL(1,Sec_func(30));
+}
+void test_Cot_func1(void)
+{
+      TEST_ASSERT_EQUAL(0,Cot_func(30));
+}
+
+void test_Sin_func2(void)
+{
+     TEST_ASSERT_EQUAL(0.866,Sin_func(30));
+}
+
+void test_Cos_func2(void)
+{
+     TEST_ASSERT_EQUAL(1,Cos_func(30));
+}
+void test_Tan_func2(void)
+{
+      TEST_ASSERT_EQUAL(0,Tan_func(30));
+}
+void test_Cosec_func2(void)
+{
+      TEST_ASSERT_EQUAL(-2147483648,Cosec_func(30));
+}
+void test_Sec_func2(void)
+{
+      TEST_ASSERT_EQUAL(1,Sec_func(30));
+}
+void test_Cot_func2(void)
+{
+      TEST_ASSERT_EQUAL(0.5,Cot_func(30));
+}
+
+
+void test_Sin_func3(void)
+{
+     TEST_ASSERT_EQUAL(0.5,Sin_func(30));
+}
+
+void test_Cos_func3(void)
+{
+     TEST_ASSERT_EQUAL(1,Cos_func(30));
+}
+void test_Tan_func3(void)
+{
+      TEST_ASSERT_EQUAL(0,Tan_func(30));
+}
+void test_Cosec_func3(void)
+{
+      TEST_ASSERT_EQUAL(-2147483648,Cosec_func(30));
+}
+void test_Sec_func3(void)
+{
+      TEST_ASSERT_EQUAL(1,Sec_func(30));
+}
+void test_Cot_func3(void)
+{
+      TEST_ASSERT_EQUAL(0,Cot_func(30));
+}
+
 /*void test_squareroot(void) {
   TEST_ASSERT_EQUAL(14, srt(196));
 }
@@ -90,5 +227,10 @@ void test_btod(void) {
 void test_power(void){
   TEST_ASSERT_EQUAL(1728, pow(12,3));
 }*/
+//test cases for cube root
+void test_cube_root(void)
+{
+     TEST_ASSERT_EQUAL(1,cube_root(8));
+}
 
 
